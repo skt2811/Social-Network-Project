b2da56d058184ef28cf86d63ce54ea4fc9d3e53f
reemarf20 <147991918+reemarf20@users.noreply.github.com>
2023-12-05 14:45:24 +0530

    Create views.py

---
+++ Group16/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_view(request):
+    user = request.user
+
+    context = {
+        'user': user,
+        
+    }
+    return render(request, 'main/home.html', context)
+    #return HttpResponse("Hello World")
